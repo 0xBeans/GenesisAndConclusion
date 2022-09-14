fdd9248d18361ce0d5ac0ff68f0bd1eb7ddce3a1
beans <certifiedbagholder@gmail.com>
2022-09-13 23:55:09 -0700

    pain

---
+++ test/Conclusion.t.sol
@@ -0,0 +1,13 @@
+// // SPDX-License-Identifier: UNLICENSED
+// pragma solidity ^0.8.13;
+
+// import "forge-std/Test.sol";
+// import "../src/Conclusion.sol";
+
+// contract ConclusionTest is Test {
+//     function setUp() public {
+//     }
+
+// }
+
+// didnt have time to write tests T_T , but it was reviewed by some big brains.
